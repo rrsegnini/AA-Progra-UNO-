a7c8572f1c280c71aad108199264b48e653bce2b
rrsegnini <roberto.t.x@hotmail.com>
2017-09-01 21:14:02 -0600

    InstAA pre alpha

---
+++ InstAA/src/Prueba.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author CASA
+ */
+
+import java.awt.image.RGBImageFilter;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class Prueba {
+    
+    /*
+    ColorFilter a = new ColorFilter(true, false, true);
+ class ColorFilter extends RGBImageFilter {
+ boolean red, green, blue;
+   
+ public ColorFilter(boolean r, boolean g, boolean b) {
+ red = r;
+ green = g;
+ blue = b;
+canFilterIndexColorModel = true;
+ }
+
+ public int filterRGB(int x, int y, int rgb) {
+ // Filter the colors
+  int r = red ? 0: ((rgb >> 16) & 0xff);  int g = green ? 0: ((rgb >> 8) & 0xff);
+    int b = blue ? 0: ((rgb >> 0) & 0xff);   // Return the result
+return (rgb & 0xff000000) | (r << 16) | (g << 8) | (b << 0);
+  }
+
+}
+    
+ */ 
+    
+    
+    public static void main(String[] args) {
+ 
+
+
+        
+        
+    }
+}
+
+
